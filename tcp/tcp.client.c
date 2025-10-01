1394d3342ab4c7bec7915f43e87c990275efec1e
DND-Dynamite <malladi.aniruddha.1999@gmail.com>
2025-10-01 16:45:47 +0530

    changes to tcp program

--- tcp/tcp.client.c
+++ tcp/tcp.client.c
@@ -1,52 +1,52 @@
+// File: client.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/socket.h>
+
 #define PORT 8080
 #define BUFFER_SIZE 1024
-int main() {
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-    char buffer[BUFFER_SIZE] = {0};
-    const char *hello = "Hello from server";
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-    
-    int opt = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("setsockopt failed");
-        exit(EXIT_FAILURE);
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
 
- 
-        perror("bind failed");
-        exit(EXIT_FAILURE);
+int main(int argc, char const *argv[]) {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFER_SIZE] = {0};
+
+    // 1. Create a socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
     }
-    if (listen(server_fd, 3) < 0) {
-        perror("listen failed");
-        exit(EXIT_FAILURE);
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 addresses from text to binary form
+    // Use "127.0.0.1" for localhost
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
     }
-    printf("Server listening on port %d...\n", PORT);
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0
-        perror("accept failed");
-        exit(EXIT_FAILURE);
+
+    // 2. Connect to the server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
     }
-    printf("Client connected.\n");
-    valread = read(new_socket, buffer, BUFFER_SIZE);
-    printf("Client message: %s\n", buffer);
-    send(new_socket, hello, strlen(hello), 0);
-    printf("Hello message sent to client.\n");
-    close(new_socket);
-    close(server_fd);
+
+    printf("Connected to server.\n");
+
+    // 3. Communicate with the server (send and read)
+    char *hello = "Hello from client";
+    send(sock, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
+
+    int valread = read(sock, buffer, BUFFER_SIZE);
+    printf("Server says: %s\n", buffer);
+
+    // 4. Close the socket
+    close(sock);
+    
     return 0;
 }
-
--- tcp/tcp.server.c
+++ tcp/tcp.server.c
@@ -1,38 +1,73 @@
+// File: server.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/socket.h>
+
 #define PORT 8080
 #define BUFFER_SIZE 1024
-int main() {
-    int sock = 0, valread;
-    struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
-    char buffer[BUFFER_SIZE] = {0};
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Socket creation error \n");
-        return -1;
-    }
-    serv_addr.sin_family = AF_INET;
 
- 
-    serv_addr.sin_port = htons(PORT);
-    
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE] = {0};
+
+    // 1. Create a socket file descriptor
+    // AF_INET: IPv4, SOCK_STREAM: TCP, 0: IP protocol
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
     }
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("connect failed");
-        return -1;
+
+    // Optional: Helps in reusing the address and port
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
     }
-    send(sock, hello, strlen(hello), 0);
-    printf("Hello message sent to server.\n");
-    valread = read(sock, buffer, BUFFER_SIZE);
-    printf("Server message: %s\n", buffer);
-    close(sock);
+
+    // 2. Bind the socket to the network address and port
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY; // Listen on all available interfaces
+    address.sin_port = htons(PORT); // Convert port to network byte order
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // 3. Listen for incoming connections
+    // The second argument is the backlog, the max number of pending connections
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d\n", PORT);
+
+    // 4. Accept an incoming connection 🤝
+    // This is a blocking call. It waits for a client to connect.
+    // It creates a new socket (`new_socket`) for this specific connection.
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Connection accepted.\n");
+
+    // 5. Communicate with the client (read and write)
+    int valread = read(new_socket, buffer, BUFFER_SIZE);
+    printf("Client says: %s\n", buffer);
+
+    char *hello = "Hello from server";
+    send(new_socket, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
+
+    // 6. Close the sockets
+    close(new_socket); // Close the connection with the client
+    close(server_fd);  // Close the listening socket
+
     return 0;
 }
-
